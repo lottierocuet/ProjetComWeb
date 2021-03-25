90edec13843ab2236634a9d8dcc82c8cb934697d
Lottie-2 <80317638+Lottie-2@users.noreply.github.com>
2021-03-25 23:53:07 +0100

    Update Modif_Info.php

--- Projet_Web/Modif_Info.php
+++ Projet_Web/Modif_Info.php
@@ -21,17 +21,52 @@
                     <p>
                         <div class = "row">
                             <div class = "col-4">
-                                <label for="promotion">Promotion :</label>
-                                <input type="number" name="promotion" min = "2007" required/> <br/>
+                                <label for="identifiant">Identifiant :</label> 
+                                <input type="text" name="identifiant" required/> <br/>
                                 <br/>
-                                <label for="adresse">Adresse :</label>
-                                <textarea rows = "2" name = "adresse"></textarea><br/>
+                                <label for="mdp">Mot de Passe :</label>
+                                <input type="password" name="mot_de_passe" required/> <br/>
+                                <br/>
+                                <label for="prenom">Prénom :</label> 
+                                <input type="text" name="prenom"/> <br/>
+                                <br/>
+                                <label for="nom">Nom :</label> 
+                                <input type="text" name="nom"/> <br/>
+                                <br/>
+                                <label for="age">Âge :</label>
+                                <input type="number" name="age"/> <br/>
+                                <br/>
+                                <label for="sexe">Sexe :</label> <br/>
+                                <input type="radio" name="sexe" id="homme"/> 
+                                <label for= "homme">Homme</label> <br/>
+                                <input type ="radio" name="sexe" id="femme"/> 
+                                <label for= "femme" >Femme</label> <br/>
+                                <input type ="radio" name="sexe" id="autre"/> 
+                                <label for= "autre" >Autre</label> <br/>
+                                <br/>
+                                <label for="promotion">Promotion :</label>
+                                <input type="number" name="promotion" min = "2007"/> <br/>
+                                <br/>
+                                <label for="nom_promotion">Nom de la promotion :</label>
+                                <input type="text" name="nom_promotion"/> <br/>
                                 <br/>
                                 <label for="email">Email :</label>
-                                <input type="email" name="email" required/> <br/>
+                                <input type="email" name="email"/> <br/>
                                 <br/>
                                 <label for="telephone">Téléphone :</label>
                                 <input type="tel" name="telephone"/> <br/>
+                                <br/>                              
+                                <label for="nationalite">Nationalité :</label>
+                                <input type="text" name="nationalite"/> <br/>
+                                <br/>
+                                <label for="code_postal">Code Postal :</label>
+                                <input type="number" name="code_postal"/> <br/>
+                                <br/>
+                                <label for="ville">Ville :</label>
+                                <input type="text" name="ville"/> <br/>
+                                <br/>
+                                <label for="adresse">Adresse :</label>
+                                <textarea class = "adresse" name = "adresse" rows = "2"></textarea><br/>
                                 <br/>
                             </div>  
                         </div>
