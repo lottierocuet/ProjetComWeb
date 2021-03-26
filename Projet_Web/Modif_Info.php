a142d85ee6fc9ba3939945a183e119a7411c6f2e
Lottie-2 <80317638+Lottie-2@users.noreply.github.com>
2021-03-26 14:53:31 +0100

    Update Modif_Info.php

--- Projet_Web/Modif_Info.php
+++ Projet_Web/Modif_Info.php
@@ -18,6 +18,7 @@
                     <div class ="Accueil col-8">Mes infos</div>
                 </div>
                 <form method="post" action="?" class ="cadre"> 
+                    <strong>Veuillez remplir la totalité des champs</strong>
                     <p>
                         <div class = "row">
                             <div class = "col-4">
@@ -28,30 +29,30 @@
                                 <input type="password" name="mot_de_passe" required/> <br/>
                                 <br/>
                                 <label for="prenom">Prénom :</label> 
-                                <input type="text" name="prenom"/> <br/>
+                                <input type="text" name="prenom" required/> <br/>
                                 <br/>
                                 <label for="nom">Nom :</label> 
-                                <input type="text" name="nom"/> <br/>
+                                <input type="text" name="nom" required/> <br/>
                                 <br/>
                                 <label for="age">Âge :</label>
                                 <input type="number" name="age"/> <br/>
                                 <br/>
                                 <label for="sexe">Sexe :</label> <br/>
-                                <input type="radio" name="sexe" id="homme"/> 
+                                <input type="radio" name="sexe" id="homme" required/> 
                                 <label for= "homme">Homme</label> <br/>
-                                <input type ="radio" name="sexe" id="femme"/> 
+                                <input type ="radio" name="sexe" id="femme" required/> 
                                 <label for= "femme" >Femme</label> <br/>
-                                <input type ="radio" name="sexe" id="autre"/> 
+                                <input type ="radio" name="sexe" id="autre" required/> 
                                 <label for= "autre" >Autre</label> <br/>
                                 <br/>
                                 <label for="promotion">Promotion :</label>
-                                <input type="number" name="promotion" min = "2007"/> <br/>
+                                <input type="number" name="promotion" min = "2007" required/> <br/>
                                 <br/>
                                 <label for="nom_promotion">Nom de la promotion :</label>
                                 <input type="text" name="nom_promotion"/> <br/>
                                 <br/>
                                 <label for="email">Email :</label>
-                                <input type="email" name="email"/> <br/>
+                                <input type="email" name="email" required/> <br/>
                                 <br/>
                                 <label for="telephone">Téléphone :</label>
                                 <input type="tel" name="telephone"/> <br/>
