d727e9c259811ced52f1352fad864c648c021b3a
Lottie-2 <80317638+Lottie-2@users.noreply.github.com>
2021-03-26 23:07:41 +0100

    Update Modif_Info.php

--- Projet_WebLottie2603/Modif_Info.php
+++ Projet_WebLottie2603/Modif_Info.php
@@ -1,3 +1,68 @@
+<?php
+
+ $bdd = new PDO('mysql:host=localhost;dbname=projetweb;charset=utf8', '001', 'ENSC2021');
+ session_start();
+if(!empty($_POST['identifiant']) && !empty($_POST['mot_de_passe'])){
+$prenom=$_POST['prenom'];
+$nom=$_POST['nom'];
+$identifiant=$_POST['identifiant'];
+$mot_de_passe=$_POST['mot_de_passe'];
+$promotion=$_POST['promotion'];
+$email=$_POST['email'];
+$nom_promotion=$_POST['nom_promotion'];
+$nationalite=$_POST['nationalite'];
+$code_postal=$_POST['code_postal'];
+$ville=$_POST['ville'];
+$adresse=$_POST['adresse'];
+$telephone=$_POST['telephone'];
+    
+$req =$bdd->prepare('UPDATE utilisateur (
+            Prenom_Utilisateur, 
+            Nom_Utilisateur, 
+            Id_Utilisateur,
+            Mot_De_Passe
+                )VALUES (:prenom,:nom, :id, :mdp)');
+$req=$req->execute(array(
+            'prenom'=>$prenom,
+            'nom'=> $nom, 
+            'id'=>$identifiant, 
+            'mdp'=>$mot_de_passe
+            ));
+
+$req2 =$bdd->prepare('UPDATE elève
+            (
+            Id_Promotion, 
+            E_mail_Eleve, 
+            Nationalite,
+            Code_Postal_Eleve, 
+            Ville_Eleve, 
+            Rue_Eleve, 
+            Telephone_Eleve
+            )VALUES (?, ?, ?, ?, ?,?,?)');
+$req2=$req2->execute(array(
+            $promotion, $email, $nationalite, $code_postal, $ville, $adresse, $telephone
+            ));
+
+$req3 =$bdd->prepare('UPDATE promotion
+            (
+                Nom_Promotion
+            )
+             VALUES (?)');
+$req3=$req3->execute(array(
+            $nom_promotion
+            ));
+
+        header('Location: Accueil_Eleve.php');
+     
+        
+     
+}
+?>
+
+
+
+
+
 <!doctype html>
 <html>
     <head>
@@ -17,21 +82,57 @@
                     <div class ="col-4"></div>
                     <div class ="Accueil col-8">Mes infos</div>
                 </div>
-                <form method="post" action="?" class ="cadre"> 
+                <form method="post" action="Modif_Info.php" class ="cadre"> 
+                    <strong>Veuillez remplir la totalité des champs</strong>
                     <p>
                         <div class = "row">
                             <div class = "col-4">
+                                <label for="identifiant">Identifiant :</label> 
+                                <input type="text" name="identifiant" required/> <br/>
+                                <br/>
+                                <label for="mdp">Mot de Passe :</label>
+                                <input type="password" name="mot_de_passe" required/> <br/>
+                                <br/>
+                                <label for="prenom">Prénom :</label> 
+                                <input type="text" name="prenom" required/> <br/>
+                                <br/>
+                                <label for="nom">Nom :</label> 
+                                <input type="text" name="nom" required/> <br/>
+                                <br/>
+                                <label for="age">Âge :</label>
+                                <input type="number" name="age"/> <br/>
+                                <br/>
+                                <label for="sexe">Sexe :</label> <br/>
+                                <input type="radio" name="sexe" id="homme" required/> 
+                                <label for= "homme">Homme</label> <br/>
+                                <input type ="radio" name="sexe" id="femme" required/> 
+                                <label for= "femme" >Femme</label> <br/>
+                                <input type ="radio" name="sexe" id="autre" required/> 
+                                <label for= "autre" >Autre</label> <br/>
+                                <br/>
                                 <label for="promotion">Promotion :</label>
                                 <input type="number" name="promotion" min = "2007" required/> <br/>
                                 <br/>
-                                <label for="adresse">Adresse :</label>
-                                <textarea rows = "2" name = "adresse"></textarea><br/>
+                                <label for="nom_promotion">Nom de la promotion :</label>
+                                <input type="text" name="nom_promotion"/> <br/>
                                 <br/>
                                 <label for="email">Email :</label>
                                 <input type="email" name="email" required/> <br/>
                                 <br/>
                                 <label for="telephone">Téléphone :</label>
                                 <input type="tel" name="telephone"/> <br/>
+                                <br/>                              
+                                <label for="nationalite">Nationalité :</label>
+                                <input type="text" name="nationalite"/> <br/>
+                                <br/>
+                                <label for="code_postal">Code Postal :</label>
+                                <input type="number" name="code_postal"/> <br/>
+                                <br/>
+                                <label for="ville">Ville :</label>
+                                <input type="text" name="ville"/> <br/>
+                                <br/>
+                                <label for="adresse">Adresse :</label>
+                                <textarea class = "adresse" name = "adresse" rows = "2"></textarea><br/>
                                 <br/>
                             </div>  
                         </div>
