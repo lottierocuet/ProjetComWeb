db8bca71c8ed350e0183113bfe7f95f7ace9ead2
Lottie-2 <80317638+Lottie-2@users.noreply.github.com>
2021-04-02 00:53:31 +0200

    Update Info.php

--- Fichiers_finaux/Info.php
+++ Fichiers_finaux/Info.php
@@ -1,26 +1,14 @@
 <?php
-
-    include 'index.php'; 
-
-
-    // ATTENTION 1: Le Id_Utilisateur n'est pas bien associé au moment de l'inscription (pour tester j'ai du connecter l'élève à l'utilisateur en allant mettre "32" dans la base pour Id_Utilisateur)
-    // ATTENTION 2: Il faut vérifier que l'utilisateur est bien connecté avant d'arriver sur cette page...
+    include_once "index.php";
+    check_gestionnaire();
+    check_connected();
     $query = $bdd->prepare('SELECT * FROM utilisateur 
         JOIN elève 
         ON elève.Id_Utilisateur = utilisateur.Id_Utilisateur
         WHERE utilisateur.Id_Utilisateur = ?');
     $query->execute([$_SESSION['identifiant']]);
     $user = $query->fetch();
-
-
-
-    
-
 ?>
-          
-
-
-
 <!doctype html>
 <html>
     <head>
@@ -30,46 +18,40 @@
         <link rel = "stylesheet" href = "style_general.css"/>
     </head>
     <body>
-        <div class = "row">
+        <div class = "row"> <!--Création d'une ligne où la navbar et le reste sont côtes à côtes-->
             <div class = "col-3">
                 <?php require_once "NavBarre_Eleve.php"; ?>
             </div>
             <div class = "col-9">
                 <h1 >Bienvenue</h1>
                 <div class ="row">
-                    <div class ="col-4"></div>
-                    <div class ="Accueil col-8">Mes infos</div>
+                    <div class ="col-md-4 col-3"></div>
+                    <div class ="Accueil col-md-8 col-9">Mes infos</div> <!--Affiche la phrase sur la droite de l'écran-->
                 </div>
                 <form method = "get" action="Info.php" class ="Infos">
-                <div class ="cadre"> 
+                <div class ="cadre"> <!-- Affiche le cadre-->
                     <h2>Promotion: </h2>
                     <?php echo $user["Promotion"]?> 
                     <br/>
-
                     <h2>
                     Email :</h2>
                     <?php echo $user["E_mail_Eleve"]?>
                     <br/>
-
                     <h2>
                     Adresse : </h2>
                     <?php echo $user["Rue_Eleve"]?> <br/>
                     <?php echo $user["Ville_Eleve"]?> <br/>
                     <?php echo $user["Code_Postal_Eleve"]?>  <br/>                 
                     <br/>
-
                     <h2>
                     Numéro Tel :</h2>
                     <?php echo $user["Telephone_Eleve"]?> <br/>
                     <br/>
-                    
-
-
                 </div>
                 </form>
                 <br/>
                 <br/>
-                <a class = "acces_modif" href= "Modif_Info.php"> Modifier ces informations </a>
+                <a class = "acces_modif" href= "Modif_Info.php"> Modifier ces informations </a> <!-- Lien avec la page où on peut modifier ses infos-->
             </div>
         </div>
     </body>
